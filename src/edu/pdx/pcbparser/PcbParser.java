a544a5d98f384520ea07389a2bfc0d7c361bc156
bewitharindam <bewitharindam@gmail.com>
2014-06-12 00:15:27 +0530

    modified add modules

--- src/edu/pdx/pcbparser/PcbParser.java
+++ src/edu/pdx/pcbparser/PcbParser.java
@@ -5,10 +5,14 @@ import java.util.*;
 
 public class PcbParser {
 	private String pcbInputFile, strLine="", layerName="", layerType="", netName="", moduleType="", moduleLayer="",moduleName="";
+	private String startIndex="";
+	private char endChar='a';
 	private List<PcbLayers> layerList = new ArrayList<PcbLayers>();
 	private List<PcbNets> netList = new ArrayList<PcbNets>();
-	private int layerId=0, numberOfNets=0, netId=0, moduleId=0, angleZ=0;
+	private List<PcbModules> moduleList = new ArrayList<PcbModules>();
+	private int layerId=0, numberOfNets=0, netId=0, moduleId=0, angleZ=0, numIndex=0;
 	private float positionX=0.00f, positionY=0.00f, componentWidth=0.00f,componentHeight=0.00f;
+	private float heightStartX=0.00f, heightStartY=0.00f, heightEndX=0.00f, heightEndY=0.00f;
 	private boolean isLayer = true, isNets = true;
 	BufferedReader br;
 	FileInputStream fstream;
@@ -23,16 +27,11 @@ public class PcbParser {
         br = new BufferedReader(new InputStreamReader(fstream));  
         while ((strLine = br.readLine()) != null) {
           if (strLine.contains("(nets ")){
-            int start = strLine.indexOf("(nets ") + 6;
-            int end = start;
-            for (final char c : strLine.substring(start).toCharArray()) {
-              if (c == ')') {
-                break;
-              }
-              ++end;
-            }				
-            numberOfNets = Integer.parseInt(strLine.substring(start, end));         
-          }
+            startIndex = "(nets ";
+            endChar = ')';
+            numIndex = 6;
+            numberOfNets = Integer.parseInt(getSubstring(startIndex,endChar,numIndex));
+          }				         
           if (strLine.contains("(layers") && isLayer){
             getLayers();         
           }
@@ -91,6 +90,44 @@ public class PcbParser {
       }
 	}
 	public void getModules(){
-    
+      try {
+        PcbModules modules = new PcbModules();
+        modules.moduleId = moduleId;
+        moduleId++;
+        modules.moduleType = getSubstring("(module ",' ',8);
+        modules.moduleLayer = getSubstring("(layer ",')',7); 
+		strLine = br.readLine();
+		String[] coords = getSubstring("(at ", ')', 4).split(" ");
+		modules.positionX = Float.parseFloat(coords[0]);
+		modules.positionY = Float.parseFloat(coords[1]);
+		if(coords.length==3)
+          modules.angleZ = Integer.parseInt(coords[2]);
+		while(true){
+          strLine = br.readLine();
+          if(strLine.contains("fp_text reference")){
+            modules.moduleName = getSubstring("reference ",' ', 10);
+          }
+          /*if(strLine.contains("fp_text value")){
+            modules.moduleNameValue = getSubstring("value ",' ', 6);
+          }*/ //future use
+          if(strLine.contains("fp_line")){
+            modules.moduleName = getSubstring("(start ",')', 7);
+          }
+          
+		}//end while(true)
+	  } catch (Exception e) {
+		e.printStackTrace();
+	  }
     }
+	public String getSubstring(String startIndex, char endChar, int numIndex){
+		int start = strLine.indexOf(startIndex) + numIndex;
+        int end = start;
+        for (final char c : strLine.substring(start).toCharArray()) {
+          if (c == endChar) {
+            break;
+          }
+          ++end;
+        }				
+      return (strLine.substring(start, end));
+	}
 }
