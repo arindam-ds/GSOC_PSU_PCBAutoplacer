fde6b971c2043902c3578891f257d689917941b4
bewitharindam <bewitharindam@gmail.com>
2014-07-15 22:27:34 +0530

    added all four partitioning

--- src/edu/pdx/partitioner/FmHeuristic.java
+++ src/edu/pdx/partitioner/FmHeuristic.java
@@ -13,24 +13,27 @@ import edu.pdx.parser.*;
  *
  */
 public class FmHeuristic {
-  String args0="";
-  String args1="";
-  int totalPrevGain = 999, totalCurrentGain=0;
+  String netlistFile="";
+  String pcbFile="";
+  int totalPrevGain, totalCurrentGain;
   int numberOfPass=1;
   int gain=0;
+  int areaConstraintMin;
   List<String> leftBucket = new ArrayList<String>();
   List<String> rightBucket = new ArrayList<String>();
+  List<String> topLeftBucket = new ArrayList<String>();
+  List<String> bottomLeftBucket = new ArrayList<String>();
+  List<String> topRightBucket = new ArrayList<String>();
+  List<String> bottomRightBucket = new ArrayList<String>();
   List<ComponentGain> compGainList = new ArrayList<ComponentGain>();
   //float verticalCutX = pcbparser.pcbBoardXmin +((pcbparser.pcbBoardXmax-pcbparser.pcbBoardXmin)/2);
-  public FmHeuristic(String args0, String args1){
-    this.args0 = args0;
-    this.args1 = args1;
+  public FmHeuristic(String netlistFile, String pcbFile){
+    this.netlistFile = netlistFile;
+    this.pcbFile = pcbFile;
   }
   public void FmVerticalPartitioner(){
-    NetlistParser netparser = new NetlistParser(args0);
+    NetlistParser netparser = new NetlistParser(netlistFile);
 	netparser.parse();  
-	int areaConstraintMin = (netparser.numberOfComponents)*(40/100);
-	//int areaConstraintMax = (netparser.numberOfComponents)*(60/100);
 	for(int i=0;i<(netparser.numberOfComponents/2);i++){
 	  leftBucket.add(netparser.compList.get(i).nameOfComp);	
 	}
@@ -41,67 +44,98 @@ public class FmHeuristic {
 	  System.out.println("test left "+leftBucket.get(j));
 	for(int j=0;j<rightBucket.size();j++)
 	  System.out.println("test right "+rightBucket.get(j));
-	while((totalCurrentGain < totalPrevGain)&&(numberOfPass<=netparser.compList.size())){
-	  numberOfPass++;
-	  totalPrevGain = totalCurrentGain;
-	  /*Gain calculation*/
-	  for(int i=0;i<netparser.compToNetList.size();i++){
-	    gain=0;
-	    totalCurrentGain=0;
-	    for(int n=0;n<netparser.compToNetList.get(i).netIdList.size();n++){
-	      for(int j=i+1;j<netparser.compToNetList.size();j++){
-	        if(netparser.compToNetList.get(j).netIdList.contains(netparser.compToNetList.get(i).netIdList.get(n))){
-	          if(leftBucket.contains(netparser.compToNetList.get(i).nameOfComp) && rightBucket.contains(netparser.compToNetList.get(j).nameOfComp)){
-	            gain++;
-	          }
-	          else if(rightBucket.contains(netparser.compToNetList.get(i).nameOfComp) && leftBucket.contains(netparser.compToNetList.get(j).nameOfComp)){
-	            gain++;
-	          }
-	          else
-	            gain--;
-	        }
-	      }
-	    }
-	    compGainList.add(new ComponentGain(netparser.netList.get(i).compName, gain));
-	  }
-	  for(int i=0;i<compGainList.size();i++){
-	    System.out.println("FmH Component: "+compGainList.get(i).nameOfComp+" FmH Gain: "+compGainList.get(i).gain);	
-	  }
-	  /*sorting component gain to get maximum gain */
-	  ComponentGain temp = new ComponentGain();
-	  for(int pass=compGainList.size()-1;pass>=0;pass--){
-	    for(int i=0;i<pass;i++){
-	      if(compGainList.get(i).gain<compGainList.get(i+1).gain){
-	        temp.nameOfComp = compGainList.get(i).nameOfComp;
-	        temp.gain = compGainList.get(i).gain;
-	        compGainList.get(i).nameOfComp = compGainList.get(i+1).nameOfComp;
-	        compGainList.get(i).gain = compGainList.get(i+1).gain;
-	        compGainList.get(i+1).nameOfComp = temp.nameOfComp;
-	        compGainList.get(i+1).gain = temp.gain;
-	      }
-	    }
-	  }
-	  /*swap the nodes considering area constraint*/
-	  for(int i=0; i<compGainList.size(); i++){
-	    if(leftBucket.contains(compGainList.get(i).nameOfComp)){
-	      if((leftBucket.size()-1)>=areaConstraintMin){
-	        leftBucket.remove(compGainList.get(i).nameOfComp);
-	        rightBucket.add(compGainList.get(i).nameOfComp);
-	        break;
-	      }          
-	    }
-	    else{
-	      if((rightBucket.size()-1)>=areaConstraintMin){
-	        rightBucket.remove(compGainList.get(i).nameOfComp);
-	        leftBucket.add(compGainList.get(i).nameOfComp);
-	        break;
-	      }	
-	    }      
-      }
-	  /*calculate total gain for current combination*/
-	  for(int i=0;i<compGainList.size();i++){
-	    totalCurrentGain = totalCurrentGain + compGainList.get(i).gain;
-      }
-    }//while loop ends
+	CorePartitioner(leftBucket, rightBucket);
+	for(int j=0;j<leftBucket.size();j++)
+      System.out.println("test left :"+leftBucket.get(j));
+    for(int j=0;j<rightBucket.size();j++)
+      System.out.println("test right :"+rightBucket.get(j));
   }//FmVerticalPartitioner() ends
+  public void FmHorizontalPartitioner(){
+    NetlistParser netparser = new NetlistParser(netlistFile);
+    netparser.parse();  
+    for(int i=0;i<leftBucket.size()/2;i++){
+      topLeftBucket.add(leftBucket.get(i));	
+    }
+    for(int i=leftBucket.size()/2;i<leftBucket.size();i++){
+      bottomLeftBucket.add(leftBucket.get(i));		
+    }
+    CorePartitioner(topLeftBucket, bottomLeftBucket);
+    for(int i=0;i<rightBucket.size()/2;i++){
+      topRightBucket.add(rightBucket.get(i));	
+    }
+    for(int i=rightBucket.size()/2;i<rightBucket.size();i++){
+      bottomRightBucket.add(rightBucket.get(i));		
+    }
+    CorePartitioner(topRightBucket, bottomRightBucket);
+  }//FmHorizontalPartitioner() ends
+  public void CorePartitioner(List<String> firstBucket, List<String> secondBucket){
+    NetlistParser netparser = new NetlistParser(netlistFile);
+    netparser.parse();  
+    totalPrevGain = 999;
+    totalCurrentGain=999;
+    areaConstraintMin = (netparser.numberOfComponents)*(40/100);
+    //int areaConstraintMax = (netparser.numberOfComponents)*(60/100);
+    while((totalCurrentGain < totalPrevGain)&&(numberOfPass<=netparser.compList.size())){
+	  numberOfPass++;
+      totalPrevGain = totalCurrentGain;
+      /*Gain calculation*/
+      for(int i=0;i<netparser.compToNetList.size();i++){
+        gain=0;
+        totalCurrentGain=0;
+        for(int n=0;n<netparser.compToNetList.get(i).netIdList.size();n++){
+          for(int j=i+1;j<netparser.compToNetList.size();j++){
+            if(netparser.compToNetList.get(j).netIdList.contains(netparser.compToNetList.get(i).netIdList.get(n))){
+              if(firstBucket.contains(netparser.compToNetList.get(i).nameOfComp) && secondBucket.contains(netparser.compToNetList.get(j).nameOfComp)){
+                gain++;
+              }
+              else if(secondBucket.contains(netparser.compToNetList.get(i).nameOfComp) && firstBucket.contains(netparser.compToNetList.get(j).nameOfComp)){
+                gain++;
+              }
+              else
+                gain--;
+            }
+          } 
+        } 
+        compGainList.add(new ComponentGain(netparser.netList.get(i).compName, gain));
+      } 
+      for(int i=0;i<compGainList.size();i++){
+        System.out.println("FmH Component: "+compGainList.get(i).nameOfComp+" FmH Gain: "+compGainList.get(i).gain);	
+      }
+      /*sorting component gain to get maximum gain */
+      ComponentGain temp = new ComponentGain();
+      for(int pass=compGainList.size()-1;pass>=0;pass--){
+        for(int i=0;i<pass;i++){
+          if(compGainList.get(i).gain<compGainList.get(i+1).gain){
+            temp.nameOfComp = compGainList.get(i).nameOfComp;
+		    temp.gain = compGainList.get(i).gain;
+		    compGainList.get(i).nameOfComp = compGainList.get(i+1).nameOfComp;
+		    compGainList.get(i).gain = compGainList.get(i+1).gain;
+            compGainList.get(i+1).nameOfComp = temp.nameOfComp;
+		    compGainList.get(i+1).gain = temp.gain;
+		  }
+		}
+      }
+      /*swap the nodes considering area constraint*/
+      for(int i=0; i<compGainList.size(); i++){
+        if(firstBucket.contains(compGainList.get(i).nameOfComp)){
+          if((firstBucket.size()-1)>=areaConstraintMin){
+            firstBucket.remove(compGainList.get(i).nameOfComp);
+		    secondBucket.add(compGainList.get(i).nameOfComp);
+		    break;
+		  }          
+		}
+		else{
+		  if((secondBucket.size()-1)>=areaConstraintMin){
+		    secondBucket.remove(compGainList.get(i).nameOfComp);
+		    firstBucket.add(compGainList.get(i).nameOfComp);
+		    break;
+		  }	
+        }      
+	  }
+      /*calculate total gain for current combination*/
+	  for(int i=0;i<compGainList.size();i++){
+        totalCurrentGain = totalCurrentGain + compGainList.get(i).gain;
+      }
+	}//while loop ends   
+  }
 }
