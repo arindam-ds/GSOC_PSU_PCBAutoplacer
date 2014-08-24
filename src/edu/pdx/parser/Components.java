ee7b999c9637b42137f48bcf410df7c7b5f50b71
bewitharindam <bewitharindam@gmail.com>
2014-08-24 20:28:59 +0530

    corrected license pointer

--- src/edu/pdx/parser/Components.java
+++ src/edu/pdx/parser/Components.java
@@ -1,11 +1,11 @@
-package edu.pdx.parser;
-
 /*
  * Copyright 2014 Arindam Bannerjee
  * This work is distributed under the terms of the "MIT license". Please see the file
  * LICENSE in this distribution for license terms.
  *
  */
+
+package edu.pdx.parser;
 
 public class Components {
 
